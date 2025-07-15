0369d01ca63075d96a1b279f6241846214c811ae
Varvara Melnyk <v.melnyk@hzdr.de>
2025-07-15 10:25:48 +0200

    config file loader

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+import yaml
+
+class AppConfig:
+    def __init__(self, path=None):
+        if path is None:
+            if os.name == 'nt':
+                path = "C:\\pymarai\\pymarai.yml"
+            else:
+                path = os.path.expanduser("/home/melnyk80/PycharmProjects/PythonProject/pymarai.yml")
+
+        with open(path, 'r') as f:
+            self.config = yaml.safe_load(f)
+
+        self.microscopes = self.config.get("microscopes", [])
+        self.default_microscope = self.config.get("default_microscope", "-")
+
+    def get_microscopes(self):
+        return self.config.get("microscopes", [])
+
+    def get_default_microscope(self):
+        return self.default_microscope
